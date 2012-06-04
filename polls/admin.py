e28e5f4b5543e48b6c32ad413a37702f5e9f4bf9
John Albright <jalbright10@gmail.com>
2012-06-04 16:07:35 -0400

    Changed order of fields and added collapsing "Date" section to the Polls admin page.

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,10 @@
 from polls.models import Poll
 from django.contrib import admin
 
-admin.site.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+admin.site.register(Poll, PollAdmin)
